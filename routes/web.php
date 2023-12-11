89fb48f453aa22b09f6f74239d2c92434181efb3
Komang Wikananda <nandasantana276@gmail.com>
2023-12-11 17:25:28 +0700

    Fixed routes

--- routes/web.php
+++ routes/web.php
@@ -36,13 +36,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::get('/games', function () {
-//     return view('games');
-// });
+Route::get('/games', function () {
+    return view('games');
+});
 
 Route::get('/before-silence', function () {
     return view('before-silence');
@@ -60,10 +60,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/dashboard', [AdminController::class, 'index']);
-    Route::get('/home', [AdminController::class, 'home']);
-    Route::get('/feedback', [AdminController::class, 'feedback']);
-    Route::get('/games', [AdminController::class, 'games']);
 });
 
 require __DIR__.'/auth.php';
