c0c250ebafb4f424a2132e6c47d99d3ae9d7a306
Komang Wikananda <nandasantana276@gmail.com>
2023-12-15 17:26:53 +0700

    Added audio player and download button on game site

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -175,6 +175,65 @@ use Illuminate\Support\Facades\Auth;
 
         </div>
 
+        <!-- <script>
+            var audioContainers = document.getElementsByClassName('audio-container');
+            console.log('audio count: ', audioContainers.length)
+            for (let i = 0; i < audioContainers.length; i++) {
+                let audio = audioContainers[i].getElementsByClassName('audio')[0];
+                let playPauseButton = audioContainers[i].getElementsByClassName('play-pause-button')[0];
+                let seekSlider = audioContainers[i].getElementsByClassName('seek-slider')[0];
+                let currentTime = audioContainers[i].getElementsByClassName('current-time')[0];
+
+                if (audio && playPauseButton && seekSlider && currentTime) {
+                    playPauseButton.addEventListener('click', function() {
+                        if (audio.paused) {
+                            audio.play();
+                            playPauseButton.classList.remove('play');
+                            playPauseButton.classList.add('pause');
+                        } else {
+                            audio.pause();
+                            playPauseButton.classList.remove('pause');
+                            playPauseButton.classList.add('play');
+                        }
+                    });
+
+                    audio.addEventListener('loadedmetadata', function(){
+                        seekSlider.max = audio.duration;
+                        seekSlider.value = 0;
+                    });
+
+                    audio.addEventListener('timeupdate', function() {
+                        seekSlider.value = audio.currentTime;
+                        currentTime.textContent = formatTime(audio.currentTime);
+                    });
+
+                    seekSlider.addEventListener('input', function() {
+                        try {
+                            audio.currentTime = seekSlider.value;
+                        } catch (e) {
+                            console.log('Failed to set audio currentTime: ', e);
+                        }
+                    });
+
+                    seekSlider.addEventListener('mousedown', function() {
+                        audio.pause();
+                    });
+
+                    seekSlider.addEventListener('mouseup', function() {
+                        audio.play();
+                    });
+                } else {
+                    console.log('One or more elements not found');
+                }
+            }
+
+            function formatTime(seconds) {
+                var minutes = Math.floor(seconds / 60);
+                seconds = Math.floor(seconds % 60);
+                return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+            }
+        </script> -->
+
         <div class='px-16 mt-10 2xl:px-64 xl:px-56 lg:px-40 md:px-32 flex flex-col'>
             <h2 class='text-3xl font-medium text-darkblue'>we are a growing team, and we need your help</h2>
             <p class='text-2xl font-light text-darkblue mt-5'>we are currently developing two games, <br> we would really appreciate if you could test and provide feedbacks for us.</p>
